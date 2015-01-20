d8dfc5dca24eac3139735ab76c00807a507ffc90
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2015-01-20 13:29:15 +0100

    first file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//directory with the jpgs made from a camera
+$from_path="camera1";
+$archie_path="arc_camera1";
+//verify there is at least 100 files
+
+//open a batch of files and compute parameters of "normal" variations
+
+//compute the hit count per jpg, write it in a log file and archive the jpg
+
+
+
+
+?>
